4dcffca76b6e34d1c8cbcc7738cda633827e2833
mallemes <dosaev.rysbek5@gmsil.com>
2023-07-29 22:33:00 +0600

    start new project BMI calc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:first_app_flutter/test.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:first_app_flutter/constants.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,39 +12,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter app',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      theme: ThemeData.dark(
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      routes: {
-        "/test": (BuildContext context) => const TestWidget(),
-        "/home": (BuildContext context) => const MyHomePage(),
-      },
-      initialRoute: "/home",
+      home: const InputPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, this.title});
-
-  final String? title;
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<InputPage> createState() => _InputPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final _controller = TextEditingController();
-  Future<void> _goTestPage()async {
-    Navigator.of(context).pushNamed("/test");
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString("key", _controller.text);
-
-  }
-
+class _InputPageState extends State<InputPage> {
+  Gender gender = Gender.female;
+  var age = 20;
+  var height = 150;
+  var weight = 0;
 
   @override
   void initState() {
@@ -54,25 +41,136 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kBG,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title??""),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('insert command name:'),
-            Padding(padding: const EdgeInsets.symmetric(horizontal: 3.3, vertical: 4), child: TextField(controller: _controller) ,)
-
-          ],
+        backgroundColor: kBG,
+        title: const Text(
+          "BMI CALCULATOR",
+          style: kBodyTextStyle,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _goTestPage,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: ReusableCard(
+                  onTap: () {
+                    setState(() {
+                      gender = Gender.male;
+                    });
+                  },
+                  color: gender == Gender.male
+                      ? kActiveCardColour
+                      : kInactiveCardColour,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 100,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "MALE",
+                        style: kBodyTextStyle,
+                      )
+                    ],
+                  ),
+                )),
+                Expanded(
+                    child: ReusableCard(
+                  onTap: () {
+                    setState(() {
+                      gender = Gender.female;
+                    });
+                  },
+                  color: gender == Gender.female
+                      ? kActiveCardColour
+                      : kInactiveCardColour,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        FontAwesomeIcons.mars,
+                        size: 100,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "FEMALE",
+                        style: kBodyTextStyle,
+                      )
+                    ],
+                  ),
+                )),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("HEIGHT", style: kBodyTextStyle,),
+                  Text(height.toString(), style: kNumberTextStyle,),
+                  Slider(
+                    min: 0, max: 220.0,
+
+                    activeColor: kBottomContainerColour, value: height.toDouble(), onChanged: (value){
+                    setState(() {
+                      height = value.round();
+                    });
+                  })
+                  ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(child: ReusableCard()),
+                Expanded(child: ReusableCard())
+              ],
+            ),
+          ),
+        ],
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+enum Gender { male, female }
+
+class ReusableCard extends StatelessWidget {
+  final Color? color;
+  final Widget? child;
+  final VoidCallback? onTap;
+
+  const ReusableCard({
+    super.key,
+    this.color = kInactiveCardColour,
+    this.child,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          child: child,
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          )),
     );
   }
 }
